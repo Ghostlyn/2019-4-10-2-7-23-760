35601824187e58b6e46820063b07da534844bea6
eleven <lyn19950705@163.com>
2019-05-29 21:40:59 +0800

    taxi billing done 1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var distance = inputs.distance
+	var parkTime = inputs.parkTime
+	var money 
+	switch(true) {
+        case distance >0 && distance <= 2: money = 6; break;
+		case distance >2 && distance <= 8: money = 6 + (distance - 2) * 0.8; break;
+		case distance >8: money = 6 + 6 * 0.8 + (distance - 8) * 0.8 * 1.5; break;
+    }
+	return Math.round(money + parkTime * 0.25)
 };
